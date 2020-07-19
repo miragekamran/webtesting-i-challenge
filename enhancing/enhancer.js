1a48930213037db0be500dc96b24a407d3ce9ff7
mirage <mirage.kamran.pkt@gmail.com>
2020-07-19 16:24:11 -0700

    project finalized

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,36 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get
 };
 
 function succeed(item) {
-  return { ...item };
+    if (item.enhancement < 20) {
+        return { ...item, enhancement: item.enhancement + 1 };
+    } else {
+        return { ...item };
+    }
 }
 
 function fail(item) {
-  return { ...item };
+    if (item.enhancement < 15) {
+        return { ...item, durability: item.durability - 5 };
+    } else if (item.enhancement === 15 || item.enhancement === 16) {
+        return { ...item, durability: item.durability - 10 };
+    } else {
+        return {
+            ...item,
+            durability: item.durability - 10,
+            enhancement: item.enhancement - 1
+        };
+    }
 }
 
 function repair(item) {
-  return { ...item };
+    return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+    return { ...item };
 }
